305a16d99d1e349d34b38e82fa5b36389c12c955
FelipeSandoval2408 <andres.sandoval.2408@gmail.com>
2024-05-16 22:00:03 -0500

    Prueba

---
+++ Proyecto/MiPrimeraPagina.js
@@ -0,0 +1,7 @@
+//Funcion en JavaScript
+const button = document.getElementById("myButton");
+
+//Evento
+document.getElementById('myButton').addEventListener('click',function() {
+    alert('Bienvenido a mi pagina web!')
+});
